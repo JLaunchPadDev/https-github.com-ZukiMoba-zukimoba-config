f61021f0e42c3f05ecfb4e03f2c7c204c3cb7b7f
Hao2505 <73099324+Hao2505@users.noreply.github.com>
2021-12-29 14:04:23 +0700

    Update config.js

--- root/airdrop/config.js
+++ root/airdrop/config.js
@@ -1,26 +1,26 @@
 {
-    cmc: {
-      contract: '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
-      title: 'this is the text',
-      description: 'this is the description',
-      ZPPrice: 0,
-      USDPrice: 100,
-      image: '/images/shop-wrapper-bg.png',
+    "cmc": {
+      "contract": '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
+      "title": 'this is the text',
+      "description": 'this is the description',
+      "ZPPrice": 0,
+      "USDPrice": 100,
+      "image": '/images/shop-wrapper-bg.png',
     },
-    community: {
-      contract: '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
-      title: 'this is the text 2',
-      description: 'this is the description 2',
-      ZPPrice: 0,
-      USDPrice: 100,
-      image: '/images/shop-wrapper-bg.png',
+    "community": {
+      "contract": '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
+      "title": 'this is the text 2',
+      "description": 'this is the description 2',
+      "ZPPrice": 0,
+      "USDPrice": 100,
+      "image": '/images/shop-wrapper-bg.png',
     },
-    community_3: {
-      contract: '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
-      title: 'this is the text 3',
-      description: 'this is the description 3',
-      ZPPrice: 0,
-      USDPrice: 100,
-      image: '/images/shop-wrapper-bg.png',
+    "community_3": {
+      "contract": '0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A',
+      "title": 'this is the text 3',
+      "description": 'this is the description 3',
+      "ZPPrice": 0,
+      "USDPrice": 100,
+      "image": '/images/shop-wrapper-bg.png',
     },
 }
