10b64355b9bbd8d0bf10d8652016d9f671556822
dilysbi <ngohung2395@gmail.com>
2021-12-29 14:16:21 +0700

    Update config.js

--- root/shopYgg/config.js
+++ root/shopYgg/config.js
@@ -1,4 +1,4 @@
 {
-  "start":             "Wed Dec 29 2021 14:30:00 GMT+0700 ",
-  "end_round_1":       "Wed Dec 29 2021 16:00:00 GMT+0700 "
+  "start":             "Wed Dec 29 2021 14:20:00 GMT+0700 ",
+  "end_round_1":       "Wed Dec 29 2021 14:22:00 GMT+0700 "
 }
