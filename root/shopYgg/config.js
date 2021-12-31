2d713b07b17bf7c03a4904cd9c6905d5dc57200e
dilysbi <ngohung2395@gmail.com>
2021-12-31 20:46:27 +0700

    Update config.js

--- root/shopYgg/config.js
+++ root/shopYgg/config.js
@@ -1,7 +1,7 @@
 {
   "zpPriceDollar": 80,
   "round": {
-    "start":             "Wed Dec 29 2021 14:00:00 GMT+0700 ",
-    "end_round_1":       "Wed Dec 29 2021 23:59:00 GMT+0700 "
+    "start":             "Dec 29 2021 14:00:00 GMT+0700",
+    "end_round_1":       "Dec 30 2022 00:00:00 GMT+0700"
   }
 }
