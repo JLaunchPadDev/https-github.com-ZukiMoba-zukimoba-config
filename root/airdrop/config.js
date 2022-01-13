60a22784bad08f71041f25ec71fbe6a5efd02ef9
Hao2505 <73099324+Hao2505@users.noreply.github.com>
2022-01-13 15:22:27 +0700

    Update config.js

--- root/airdrop/config.js
+++ root/airdrop/config.js
@@ -1,26 +1,10 @@
 {
-    "cmc": {
-      "contract": "0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A",
-      "title": "this is the text",
-      "description": "this is the description",
+    "airdrop-nft": {
+      "contract": "0xdBe25CEE1470a64166df0E069286ecdcEc130615",
+      "title": "TREASURE CHEST FOR WHITELIST AIRDROP",
+      "description": "NFTs - Claim Blindbox",
       "ZPPrice": 0,
-      "USDPrice": 100,
-      "image": "/images/shop-wrapper-bg.png"
-    },
-    "community": {
-      "contract": "0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A",
-      "title": "this is the text 2",
-      "description": "this is the description 2",
-      "ZPPrice": 0,
-      "USDPrice": 100,
-      "image": "/images/shop-wrapper-bg.png"
-    },
-    "community_3": {
-      "contract": "0xd9EA16cD1b463cE837F605C912D0594EeAe51c7A",
-      "title": "this is the text 3",
-      "description": "this is the description 3",
-      "ZPPrice": 0,
-      "USDPrice": 100,
+      "USDPrice": 0,
       "image": "/images/shop-wrapper-bg.png"
     }
 }
