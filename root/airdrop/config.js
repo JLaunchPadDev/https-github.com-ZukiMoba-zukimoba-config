53176408349ecf8463d714d1d66edcde183fc1e0
dilysbi <ngohung2395@gmail.com>
2022-01-16 22:46:08 +0700

    Update config.js

--- root/airdrop/config.js
+++ root/airdrop/config.js
@@ -6,7 +6,7 @@
       "ZPPrice": 0,
       "USDPrice": 0,
       "image": "/images/shop-wrapper-bg.png",
-      "cowdown": {
+      "countdown": {
         "start": "Dec 24 2021 16:00:00 GMT",
         "end_round_1": "Dec 30 2022 00:00:00 GMT"
       }
