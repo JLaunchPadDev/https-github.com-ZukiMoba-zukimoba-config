35069fb5cd5c66617fef93ade011bd69a92e9278
dilysbi <ngohung2395@gmail.com>
2022-01-16 22:44:58 +0700

    Update config.js

--- root/airdrop/config.js
+++ root/airdrop/config.js
@@ -5,6 +5,10 @@
       "description": "NFTs - Claim Blindbox",
       "ZPPrice": 0,
       "USDPrice": 0,
-      "image": "/images/shop-wrapper-bg.png"
+      "image": "/images/shop-wrapper-bg.png",
+      "cowdown": {
+        "start": "Dec 24 2021 16:00:00 GMT",
+        "end_round_1": "Dec 30 2022 00:00:00 GMT"
+      }
     }
 }
